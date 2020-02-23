7a3bdd277484a4c2154be968bf74dfa4573a0e22
aguiar <aguiar.eduardo@gmail.com>
2020-02-23 20:49:08 -0300

    Obtendo um usuario por ID. Criacao de classes de tratamento de erro

--- src/main/java/com/borba/springbootmongo/services/UserService.java
+++ src/main/java/com/borba/springbootmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.borba.springbootmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.borba.springbootmongo.domain.User;
 import com.borba.springbootmongo.repository.UserRepository;
+import com.borba.springbootmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,15 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+//		if (user == null) {
+//			throw new ObjectNotFoundException("Objeto não encontrado");
+//		}
+//		return user;
+		
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
---
+++ src/main/java/com/borba/springbootmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package com.borba.springbootmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
