2b27aecf0f721d987f46d7a7d6d405f4c009a042
aguiar <aguiar.eduardo@gmail.com>
2020-02-23 19:45:38 -0300

    Criando Classe de Configuration, executa ao iniciar a aplicacao

---
+++ src/main/java/com/borba/springbootmongo/config/Instantiation.java
@@ -0,0 +1,34 @@
+package com.borba.springbootmongo.config;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.borba.springbootmongo.domain.User;
+import com.borba.springbootmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+//		userRepository.save(maria);
+		userRepository.insert(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
