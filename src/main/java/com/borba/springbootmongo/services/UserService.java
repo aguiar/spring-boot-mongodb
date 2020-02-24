7cb2ebd6b8fbcb30a4908661461cc8b45876919c
aguiar <aguiar.eduardo@gmail.com>
2020-02-24 12:03:01 -0300

    Inserindo usuario com POST

--- src/main/java/com/borba/springbootmongo/services/UserService.java
+++ src/main/java/com/borba/springbootmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.borba.springbootmongo.domain.User;
+import com.borba.springbootmongo.dto.UserDTO;
 import com.borba.springbootmongo.repository.UserRepository;
 import com.borba.springbootmongo.services.exception.ObjectNotFoundException;
 
@@ -28,5 +29,13 @@ public class UserService {
 		
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
