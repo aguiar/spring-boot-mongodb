517f118add5b77a266d7a691a0848a37d48ff154
aguiar <aguiar.eduardo@gmail.com>
2020-02-25 11:17:00 -0300

    Criando entity Post com user aninhado

--- src/main/java/com/borba/springbootmongo/config/Instantiation.java
+++ src/main/java/com/borba/springbootmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.borba.springbootmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.borba.springbootmongo.domain.Post;
 import com.borba.springbootmongo.domain.User;
+import com.borba.springbootmongo.repository.PostRepository;
 import com.borba.springbootmongo.repository.UserRepository;
 
 @Configuration
@@ -15,18 +19,28 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		
 //		userRepository.save(maria);
 		userRepository.insert(Arrays.asList(maria, alex, bob));
+		postRepository.insert(Arrays.asList(post1, post2));
 		
 	}
 
