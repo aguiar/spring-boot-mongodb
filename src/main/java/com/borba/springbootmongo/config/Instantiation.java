dc9cb8c06e0b8aed5b26339640f479ae457c2896
aguiar <aguiar.eduardo@gmail.com>
2020-02-25 11:31:45 -0300

    Gravando dados na colecao de Post(tabela post) com dados do author com a criacao de uma classe DTO do author (AuthorDTO)

--- src/main/java/com/borba/springbootmongo/config/Instantiation.java
+++ src/main/java/com/borba/springbootmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.borba.springbootmongo.domain.Post;
 import com.borba.springbootmongo.domain.User;
+import com.borba.springbootmongo.dto.AuthorDTO;
 import com.borba.springbootmongo.repository.PostRepository;
 import com.borba.springbootmongo.repository.UserRepository;
 
@@ -35,11 +36,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 //		userRepository.save(maria);
 		userRepository.insert(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
 		postRepository.insert(Arrays.asList(post1, post2));
 		
 	}
